04f3e9112989705cd19db7615c9ff8a4523e51c2
rebeccapeltz <rebeccapeltz@gmail.com>
2023-05-16 15:19:39 -0700

    add export to nextConfig

--- next.config.js
+++ next.config.js
@@ -2,3 +2,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   })
   module.exports = withBundleAnalyzer({})
+
+  /**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+      },
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  };
+   
+  module.exports = nextConfig;
