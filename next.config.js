9626fbfdcfcfd23dc3ddd0391115c65d74d76759
rebeccapeltz <rebeccapeltz@gmail.com>
2023-05-17 11:45:12 -0700

    add standalone feature to next.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
       },
+      output:"standalone",
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
