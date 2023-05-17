534b53093daa4c250a888506ddd995152bb2e8ca
rebeccapeltz <rebeccapeltz@gmail.com>
2023-05-17 14:29:12 -0700

    try static option

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
-  module.exports = withBundleAnalyzer({})
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withBundleAnalyzer({});
 
-  /**
+/**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-      },
-      output:"standalone",
-    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-    // trailingSlash: true,
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  };
-   
-  module.exports = nextConfig;
+  // images: {
+  //   unoptimized: true,
+  // },
+  // experimental: {
+  //   outputFileTracingIncludes: {
+  //     outputFileTracingRoot: './',
+  //   },
+  // },
+  trailingSlash: true,
+  // output: "standalone",
+  // distDir: 'out',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
