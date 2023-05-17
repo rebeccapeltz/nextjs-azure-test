d2f062becc1da5dbfead2573673c5cbe8d27abff
rebeccapeltz <rebeccapeltz@gmail.com>
2023-05-17 15:05:32 -0700

    cleanup nextconfigjs

--- next.config.js
+++ next.config.js
@@ -10,18 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // experimental: {
-  //   outputFileTracingIncludes: {
-  //     outputFileTracingRoot: './',
-  //   },
-  // },
   trailingSlash: true,
   output: "export",
-  // distDir: 'out',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
